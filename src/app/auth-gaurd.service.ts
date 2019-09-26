6bc879662ced129cf00cecba5e991cc9bfd15e6a
arnabbhat82 <arnabbhat@gmail.com>
2019-09-26 15:39:10 +0530

    auth-gaurd.service applying canActivate method

--- src/app/auth-gaurd.service.ts
+++ src/app/auth-gaurd.service.ts
@@ -2,12 +2,25 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
+  Router,
 } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthGaurd implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
-  ): Observable<boolean> | Promise<boolean> | boolean {}
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) {
+        return true;
+      } else {
+        this.router.navigate(['/']);
+      }
+    });
+  }
 }
