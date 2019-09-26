3845fcfa08641d333fd5a6211dcf25af77ce8c66
arnabbhat82 <arnabbhat@gmail.com>
2019-09-26 17:15:36 +0530

    can deactivate need to learn from gutu

---
+++ src/app/child/can-deactivate-guard.service.ts
@@ -0,0 +1,9 @@
+// import { Observable } from 'rxjs/Observable'
+// import { CanDeactivate } from '@angular/router';
+
+// export interface CanComponentDeactivate() {
+//   canDeactivate: () => Observable<boolean> | Promise<boolean> | bolean;
+// }
+// export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate> {
+
+// }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+  styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent implements OnInit {
+  changesSaved = false;
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  savedChanges() {
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
-
 }
