09bbc1a0927b1be87c550928e8ee4d130d320fbc
arnabbhat82 <arnabbhat@gmail.com>
2019-09-26 16:15:56 +0530

    canActivateChild method

--- src/app/auth-gaurd.service.ts
+++ src/app/auth-gaurd.service.ts
@@ -3,13 +3,14 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router,
+  CanActivateChild,
 } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class AuthGaurd implements CanActivate {
+export class AuthGaurd implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -22,5 +23,11 @@ export class AuthGaurd implements CanActivate {
         this.router.navigate(['/']);
       }
     });
+  }
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
   }
 }
