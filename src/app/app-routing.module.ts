342d5ed478f0b7e0efd61f59ebf1e58f9745a573
arnabbhat82 <arnabbhat@gmail.com>
2019-09-26 15:51:02 +0530

    parent is being restricted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ParentComponent } from './parent/parent.component';
 import { AuthGaurd } from './auth-gaurd.service';
+import { ChildComponent } from './child/child.component';
 
 const routes: Routes = [
   { path: 'parent', canActivate: [AuthGaurd], component: ParentComponent },
+  { path: 'child', component: ChildComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
+import { AuthService } from './auth.service';
+import { AuthGaurd } from './auth-gaurd.service';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ParentComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ParentComponent, ChildComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [AuthService, AuthGaurd],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
